6808b9ccb9cb1f3af26e82adc706d20f94654217
Ever <everstarckone@protonmail.com>
2024-09-24 15:52:22 -0600

    refactor: pass string cors instead of array

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -23,11 +23,14 @@ async def log_requests(request: Request, call_next):
     logger.info(f"Response status: {response.status_code}")
     return response
 
+print("🚀 ~ settings.BACKEND_CORS_ORIGINS:", settings.BACKEND_CORS_ORIGINS)
+print([str(origin).strip("/") for origin in settings.BACKEND_CORS_ORIGINS])
+
 # Set all CORS enabled origins
 if settings.BACKEND_CORS_ORIGINS:
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS] + ["http://localhost:4321"],
+        allow_origins=[str(origin).strip("/") for origin in settings.BACKEND_CORS_ORIGINS] + ["http://localhost:4321", "https://dev.cleverbill.ing"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
