129fc824ae6376a99b37bcd6f72ea91e46d81a1b
Ever <everstarckone@protonmail.com>
2024-12-21 22:23:52 -0600

    feat: add better ai img type validation

--- backend/app/app/api/api_v1/endpoints/ai.py
+++ backend/app/app/api/api_v1/endpoints/ai.py
@@ -2,9 +2,9 @@ from typing import Any
 
 import tempfile
 import os
-from datetime import datetime
 from fastapi import APIRouter, Depends, HTTPException, File, UploadFile, status
 from sqlalchemy.ext.asyncio import AsyncSession
+import filetype
 
 from app import crud, models, schemas
 from app.api import deps
@@ -15,6 +15,31 @@ router = APIRouter()
 ocr = OCRHelper(settings.OPENAI_API_KEY)
 
 
+# TODO: Validate size: https://github.com/fastapi/fastapi/issues/362
+def validate_file_type(file: UploadFile) -> None:
+    # FILE_SIZE = 2097152 # 2MB
+
+    accepted_file_types = ["image/png", "image/jpeg", "image/jpg", "image/heic", "image/heif", "image/heics", "png",
+                          "jpeg", "jpg", "heic", "heif", "heics"]
+    file_info = filetype.guess(file.file)
+    if file_info is None:
+        raise HTTPException(
+            status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+            detail="Unable to determine file type",
+        )
+
+    detected_content_type = file_info.extension.lower()
+
+    if (
+        file.content_type not in accepted_file_types
+        or detected_content_type not in accepted_file_types
+    ):
+        raise HTTPException(
+            status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+            detail="Unsupported file type",
+        )
+
+
 @router.post("/ocr", response_model=Any)
 async def ocr_image(
     *,
@@ -22,11 +47,7 @@ async def ocr_image(
     image: UploadFile = File(...),
     current_user: models.User = Depends(deps.get_current_active_user),
 ) -> Any:
-    if not image.content_type.startswith("image/"):
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="File uploaded is not an image"
-        )
+    validate_file_type(image)
 
     try:
         # Save uploaded file temporarily
@@ -36,35 +57,12 @@ async def ocr_image(
             temp_file.flush()
 
             transaction = await ocr.analyze_image(temp_file.name)
-            print("🚀 ~ transaction:", transaction)
-            # transaction = '{\n  "transactions": [\n    {\n      "amount": 430.00,\n      "date": "2024-12-15",\n      "place": "Camarones Vvalos",\n      "description": "Hamburguesa de pescado x1, Tostada de ceviche x2",\n      "category": "Alimentos y bebidas",\n      "subcategory": "Comida rápida",\n      "type": "expense"\n    }\n  ]\n}'
-
             parsed_transaction = await ocr.parse_response(db=db, owner_id=current_user.id, response_text=transaction)
-            print("🚀 ~ parsed_transaction:", parsed_transaction)
 
-            return parsed_transaction
-
-            # # Create transaction in database
-            # db_transaction = await crud.transaction.create(
-            #     db=db,
-            #     obj_in=schemas.TransactionCreate(
-            #         user_id=current_user.id,
-            #         type=transaction.type,
-            #         amount=transaction.amount,
-            #         date=transaction.date or datetime.now(),
-            #         category=transaction.category,
-            #         subcategory=transaction.subcategory,
-            #         place=transaction.place,
-            #         description=transaction.description
-            #     )
-            # )
-
-            # Clean up temp file
+            # Remove temporary file
             os.unlink(temp_file.name)
-
-            # return db_transaction
+            return parsed_transaction
     except Exception as e:
-        print("🚀 ~ e:", e)
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=e
